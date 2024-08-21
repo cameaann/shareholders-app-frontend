a1f6a76f2d5aaeffbdb82c10d84b1b16e127eba4
Anna <cameaann@gmail.com>
2024-08-21 09:13:57 +0300

    adding Shareholders component

--- src/components/Shareholders.jsx
+++ src/components/Shareholders.jsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from "react";
 import { getShareholders } from "../services/shareholdersService";
 
 const Shareholders = () =>{
-    const [status, setStatus ] = useState([]);
+    const [shareholdersList, setShareholders ] = useState([]);
 
     useEffect(() => {
         getShareholders().then(res => {
-            setStatus(res);
+            setShareholders(res);
         })
         .catch((error)=>{
             console.error(error);
@@ -15,8 +15,8 @@ const Shareholders = () =>{
 
     return(
         <div>
-            <h1>Health Check Status:</h1>
-            <p>{status}</p>
+            <h1>List of shareholders:</h1>
+            <ul>{shareholdersList. map(x => <li key={x.id}>{x}</li>)}</ul>
         </div>
     )
 
