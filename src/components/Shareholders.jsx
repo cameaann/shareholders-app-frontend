3f7a6e9eb06901f26e67f7ac40dec3887f29f816
Anna <cameaann@gmail.com>
2024-08-21 14:21:41 +0300

    adding mocked array to shareholders

--- src/components/Shareholders.jsx
+++ src/components/Shareholders.jsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
 import { getShareholders } from "../services/shareholdersService";
 
+
+const initialData = [{id:'bhsd5', name: "Alex"}, {id:'bhso9', name: "Kristofer"}, {id:'jj2o9', name: "Alexander"}];
 const Shareholders = () =>{
-    const [shareholdersList, setShareholders ] = useState([]);
+    const [shareholdersList, setShareholders ] = useState(initialData);
 
     useEffect(() => {
         getShareholders().then(res => {
-            setShareholders(res);
+            if (Array.isArray(res)) {
+                setShareholders(res);
+            }
         })
         .catch((error)=>{
             console.error(error);
@@ -16,7 +20,7 @@ const Shareholders = () =>{
     return(
         <div>
             <h1>List of shareholders:</h1>
-            <ul>{shareholdersList. map(person => <li key={person.id}>{person.name}</li>)}</ul>
+            <ul>{shareholdersList.map(person => <li key={person.id}>{person.name}</li>)}</ul>
         </div>
     )
 
