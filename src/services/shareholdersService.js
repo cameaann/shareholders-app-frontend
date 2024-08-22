de6c6129995279ca46e1101c43049d713d527c17
Anna <cameaann@gmail.com>
2024-08-22 16:28:19 +0300

    adding link to azure container

--- src/services/shareholdersService.js
+++ src/services/shareholdersService.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
+const healthUrl = "https://shareholderscontainer.livelyrock-36ef92b3.germanywestcentral.azurecontainerapps.io/health";
+const shareHoldersUrl = "https://shareholderscontainer.livelyrock-36ef92b3.germanywestcentral.azurecontainerapps.io/api/shareholders";
+
 const devUrl = "http://localhost:8080/health";
-const shareHoldersUrl = "http://localhost:8080/api/shareholders";
+const shareHoldersUrlDev = "http://localhost:8080/api/shareholders";
+
 
 
 const getStatus = async () =>{
-    const res = await axios.get(devUrl);
+    const res = await axios.get(healthUrl);
     return res.data;
 }
 
