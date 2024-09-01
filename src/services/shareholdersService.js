5ffb43569a0faebf8de4353798b176e67d0e8a5d
Anna <cameaann@gmail.com>
2024-09-01 23:32:14 +0300

    adding saveShareholder method to shareholderService

--- src/services/shareholdersService.js
+++ src/services/shareholdersService.js
@@ -1,17 +1,58 @@
-import axios from 'axios';
+import axios from "axios";
 
 const devUrl = "http://localhost:8080/health";
 const shareHoldersUrl = "http://localhost:8080/api/shareholders";
 
+const getStatus = async () => {
+  const res = await axios.get(devUrl);
+  return res.data;
+};
 
-const getStatus = async () =>{
-    const res = await axios.get(devUrl);
-    return res.data;
-}
+const getShareholders = async () => {
+  const res = await axios.get(shareHoldersUrl);
+  return res.data;
+};
 
-const getShareholders = async () =>{
-    const res = await axios.get(shareHoldersUrl);
-    return res.data;
-}
+const saveShareholder = async (formData) => {
 
-export { getShareholders, getStatus}
+  const payload = {
+    name: formData.name,
+    personalIdOrCompanyId: formData.personalId,
+    placeOfResidenceOrHeadquarters: formData.city,
+    address: formData.address,
+    emailAddress: formData.email,
+    phoneNumber: formData.phoneNumber,
+    bankAccountNumber: formData.bankAccountNumber,
+  };
+
+  if(formData.shares.quantity > 0){
+    console.log(formData.shares.quantity);
+    payload.shares = [
+        {
+          alkaen: formData.shares.start,
+          paattyen: formData.shares.end
+        },
+      ]
+  }
+  console.log(JSON.stringify(payload));
+  try {
+    const response = await axios.post(shareHoldersUrl, payload, {
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.status === 200) {
+      alert("Shareholder saved successfully!");
+      return true;
+      // Optionally, clear form here
+    } else {
+      alert("Failed to save shareholder");
+    }
+  } catch (error) {
+    console.error("Error saving shareholder:", error);
+    alert("An error occurred while saving the shareholder");
+    return false;
+  }
+};
+
+export { getShareholders, getStatus, saveShareholder };
