bb0cb22f4887d0d2bd5203d939cd1d78a8882f13
Anna <cameaann@gmail.com>
2024-09-12 01:24:17 +0300

    changing payload object

--- src/services/shareholdersService.js
+++ src/services/shareholdersService.js
@@ -16,18 +16,21 @@ const getShareholders = async () => {
 const saveShareholder = async (formData) => {
 
   const payload = {
-    name: formData.name,
-    personalIdOrCompanyId: formData.personalId,
-    placeOfResidenceOrHeadquarters: formData.city,
-    address: formData.address,
-    emailAddress: formData.email,
-    phoneNumber: formData.phoneNumber,
-    bankAccountNumber: formData.bankAccountNumber,
+    shareholder : {
+      name: formData.name,
+      personalIdOrCompanyId: formData.personalId,
+      placeOfResidenceOrHeadquarters: formData.city,
+      address: formData.address,
+      emailAddress: formData.email,
+      phoneNumber: formData.phoneNumber,
+      bankAccountNumber: formData.bankAccountNumber,
+    },
+    sharesQuantity: 0
   };
 
-  if(formData.shares.quantity > 0){
-    console.log(formData.shares.quantity);
-    payload.quantity = parseInt(formData.quantity);
+  if(formData.quantity > 0){
+    console.log(formData.quantity);
+    payload.sharesQuantity = parseInt(formData.quantity);
   }
   console.log(JSON.stringify(payload));
   try {
