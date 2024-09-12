63a8d834419bc750fb1f1ac1fb9468abfc43caae
Anna <cameaann@gmail.com>
2024-09-12 22:34:21 +0300

    adding onBlur to MaskAdapter

--- src/components/TextMaskAdapter.jsx
+++ src/components/TextMaskAdapter.jsx
@@ -1,25 +1,29 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { IMaskInput } from 'react-imask';
+import * as React from "react";
+import PropTypes from "prop-types";
+import { IMaskInput } from "react-imask";
 
-export const TextMaskAdapter = React.forwardRef(function TextMaskAdapter(props, ref) {
-    const { onChange, ...other } = props;
-    return (
-      <IMaskInput
-        {...other}
-        mask="(+000) 000-000000"
-        definitions={{
-          '#': /[1-9]/,
-        }}
-        inputRef={ref}
-        onAccept={(value) => onChange({ target: { name: props.name, value } })}
-        overwrite
-      />
-    );
-  });
-  
-  TextMaskAdapter.propTypes = {
-    name: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-  };
- 
+export const TextMaskAdapter = React.forwardRef(function TextMaskAdapter(
+  props,
+  ref
+) {
+  const { onChange, onBlur, ...other } = props;
+  return (
+    <IMaskInput
+      {...other}
+      mask="(+000) 000-000000"
+      definitions={{
+        "#": /[1-9]/,
+      }}
+      inputRef={ref}
+      onAccept={(value) => onChange({ target: { name: props.name, value } })}
+      onBlur={onBlur}
+      overwrite
+    />
+  );
+});
+
+TextMaskAdapter.propTypes = {
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func,
+};
