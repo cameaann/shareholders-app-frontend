063ec33cdf030440b0f6fe42439497f9556e0c47
Anna <cameaann@gmail.com>
2024-09-18 23:08:12 +0300

    adding useMediaQuery hook to the form

--- src/components/CreateOrEditShareholderForm.jsx
+++ src/components/CreateOrEditShareholderForm.jsx
@@ -12,11 +12,14 @@ import { TextMaskAdapter } from "./TextMaskAdapter";
 import { saveShareholder } from "../services/shareholdersService";
 import { useEffect, useState } from "react";
 import { validateField } from "../functions/validateForm";
+import { useMediaQuery } from "@mui/material";
 
 const CreateOrEditShareholderForm = ({
   sharesTotalQuantity,
   onAddingMainShareholder,
 }) => {
+  const isSmallScreen = useMediaQuery("(max-width: 660px)");
+  // const isMediumScreen = useMediaQuery("(max-width: 870px)");
   const nameProps = useFormInput();
   const emailProps = useFormInput();
   const phoneNumberProps = useFormInput("(100) 000-0000");
@@ -60,7 +63,7 @@ const CreateOrEditShareholderForm = ({
 
   const handleChange = (props, field) => (event) => {
     props.onChange(event); // Update the value using custom hook
-   
+
     if (touched[field]) {
       handleValidation(field, event.target.value);
     }
@@ -115,14 +118,27 @@ const CreateOrEditShareholderForm = ({
       alignContent="center"
       sx={{ margin: "70px auto", width: "100%" }}
     >
-      <Typography sx={{ fontSize: 22, fontWeight: "bold" }} alignSelf="center">
-        Lisää uusi omistaja
-      </Typography>
-      <Stack flexDirection="row" sx={{ gap: 2 }} justifyContent="center">
-        <FormControl sx={{ mt: 4 }} error={!!errors.name}>
+      {!isSmallScreen ? (
+        <Typography
+          sx={{ fontSize: 22, fontWeight: "bold" }}
+          alignSelf="center"
+        >
+          Lisää uusi omistaja
+        </Typography>
+      ) : (
+        ""
+      )}
+
+      <Stack
+        flexDirection={isSmallScreen ? "column" : "row"}
+        sx={{ gap: 2 }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <FormControl sx={{ mt: 4, width: isSmallScreen ? "290px" : "300px" }} error={!!errors.name} >
           <FormLabel>Nimi</FormLabel>
           <Input
-            sx={{ width: "300px" }}
+            sx={{ width: isSmallScreen ? "290px" : "300px" }}
             placeholder="Nimi"
             value={nameProps.value}
             onChange={handleChange(nameProps, "name")}
@@ -130,10 +146,10 @@ const CreateOrEditShareholderForm = ({
           />
           {!!errors.name && <FormHelperText>{errors.name}</FormHelperText>}
         </FormControl>
-        <FormControl sx={{ mt: 4 }} error={!!errors.personalId}>
+        <FormControl sx={{ mt: 4, width: isSmallScreen ? "290px" : "300px" }} error={!!errors.personalId}>
           <FormLabel>Hetu/Y-tunnus</FormLabel>
           <Input
-            sx={{ width: "300px" }}
+            sx={{ width: isSmallScreen ? "290px" : "300px" }}
             placeholder="Y-tunnus"
             value={personalIdProps.value}
             onChange={handleChange(personalIdProps, "personalId")}
@@ -144,8 +160,13 @@ const CreateOrEditShareholderForm = ({
           )}
         </FormControl>
       </Stack>
-      <Stack flexDirection="row" sx={{ gap: 2 }} justifyContent="center">
-        <FormControl sx={{ mt: 3 }} error={!!errors.phoneNumber}>
+      <Stack
+        flexDirection={isSmallScreen ? "column" : "row"}
+        sx={{ gap: 2 }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <FormControl sx={{ mt: 3, width: isSmallScreen ? "290px" : "300px" }} error={!!errors.phoneNumber}>
           <FormLabel>Puhelin numero</FormLabel>
           <Input
             value={phoneNumberProps.value}
@@ -155,7 +176,7 @@ const CreateOrEditShareholderForm = ({
             }}
             name="phoneNumber"
             placeholder="Placeholder"
-            sx={{ width: "300px" }}
+            sx={{ width: isSmallScreen ? "290px" : "300px" }}
             slotProps={{ input: { component: TextMaskAdapter } }}
             onBlur={handleBlur(phoneNumberProps, "phoneNumber")}
           />
@@ -164,10 +185,10 @@ const CreateOrEditShareholderForm = ({
             <FormHelperText>{errors.phoneNumber}</FormHelperText>
           )}
         </FormControl>
-        <FormControl sx={{ mt: 3 }} error={!!errors.email}>
+        <FormControl sx={{ mt: 3, width: isSmallScreen ? "290px" : "300px" }} error={!!errors.email}>
           <FormLabel>Sähköposti osoite</FormLabel>
           <Input
-            sx={{ width: "300px" }}
+            sx={{ width: isSmallScreen ? "290px" : "300px" }}
             placeholder="Sähköposti osoite"
             value={emailProps.value}
             onChange={handleChange(emailProps, "email")}
@@ -176,11 +197,16 @@ const CreateOrEditShareholderForm = ({
           {!!errors.email && <FormHelperText>{errors.email}</FormHelperText>}
         </FormControl>
       </Stack>
-      <Stack flexDirection="row" sx={{ gap: 2 }} justifyContent="center">
-        <FormControl sx={{ mt: 3 }} error={!!errors.city}>
+      <Stack
+        flexDirection={isSmallScreen ? "column" : "row"}
+        sx={{ gap: 2 }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <FormControl sx={{ mt: 3, width: isSmallScreen ? "290px" : "300px" }} error={!!errors.city}>
           <FormLabel>Kotipaikka</FormLabel>
           <Input
-            sx={{ width: "300px" }}
+            sx={{ width: isSmallScreen ? "290px" : "300px" }}
             placeholder="Kotipaikka"
             value={cityProps.value}
             onChange={handleChange(cityProps, "city")}
@@ -188,13 +214,13 @@ const CreateOrEditShareholderForm = ({
           />
           {!!errors.city && <FormHelperText>{errors.city}</FormHelperText>}
         </FormControl>
-        <FormControl sx={{ mt: 3 }} error={!!errors.address}>
+        <FormControl sx={{ mt: 3, width: isSmallScreen ? "290px" : "300px" }} error={!!errors.address}>
           <FormLabel>Postiosoite</FormLabel>
           <Input
             value={addressProps.value}
             onChange={handleChange(addressProps, "address")}
             placeholder="Postiosoite"
-            sx={{ width: "300px" }}
+            sx={{ width: isSmallScreen ? "290px" : "300px" }}
             onBlur={handleBlur(addressProps, "address")}
           />
           {!!errors.address && (
@@ -202,14 +228,19 @@ const CreateOrEditShareholderForm = ({
           )}
         </FormControl>
       </Stack>
-      <Stack flexDirection="row" sx={{ gap: 2 }} justifyContent="center">
-        <FormControl sx={{ mt: 3 }} error={!!errors.bankAccountNumber}>
+      <Stack
+        flexDirection={isSmallScreen ? "column" : "row"}
+        sx={{ gap: 2 }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <FormControl sx={{ mt: 3, width: isSmallScreen ? "290px" : "300px" }} error={!!errors.bankAccountNumber}>
           <FormLabel>Tili numero</FormLabel>
           <Input
             value={bankAccountNumberProps.value}
             onChange={handleChange(bankAccountNumberProps, "bankAccountNumber")}
             placeholder="Tili numero"
-            sx={{ width: "300px" }}
+            sx={{ width: isSmallScreen ? "290px" : "300px" }}
             onBlur={handleBlur(bankAccountNumberProps, "bankAccountNumber")}
           />
           {!!errors.bankAccountNumber && (
@@ -217,12 +248,14 @@ const CreateOrEditShareholderForm = ({
           )}
         </FormControl>
         {sharesTotalQuantity > 0 ? (
-          <Typography sx={{ width: "300px" }}></Typography>
+          <Typography
+            sx={{ width: isSmallScreen ? "290px" : "300px" }}
+          ></Typography>
         ) : (
-          <FormControl sx={{ mt: 3 }}>
+          <FormControl sx={{ mt: 3, width: isSmallScreen ? "290px" : "300px" }}>
             <FormLabel>Lisää osakeet (Kpl)</FormLabel>
             <Input
-              sx={{ width: "300px" }}
+              sx={{ width: isSmallScreen ? "290px" : "300px" }}
               placeholder="0"
               value={shareQuantity.value}
               onChange={shareQuantity.onChange}
@@ -234,13 +267,21 @@ const CreateOrEditShareholderForm = ({
       <Stack sx={{ mt: 7 }}>
         <Stack flexDirection="row" sx={{ gap: 2 }} justifyContent="center">
           <Button
-            sx={{ backgroundColor: "#317A26", width: "300px", mt: 1 }}
+            sx={{
+              backgroundColor: "#317A26",
+              width: isSmallScreen ? "290px" : "300px",
+              mt: 1,
+            }}
             onClick={handleSubmit}
             disabled={isSubmitDisabled}
           >
             Submit
           </Button>
-          <Typography sx={{ width: "300px" }}></Typography>
+          {!isSmallScreen ? (
+            <Typography sx={{ width: "300px" }}></Typography>
+          ) : (
+            ""
+          )}
         </Stack>
       </Stack>
     </Stack>
