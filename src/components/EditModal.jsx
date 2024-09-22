9321c38d4429281bf3e81dd45a673f419643a4bf
Anna <cameaann@gmail.com>
2024-09-22 23:18:54 +0300

    adding CreateOrEditShareholderForm to EditModal

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -5,12 +5,14 @@ import { SharesQuantityContext } from "./SharesQuantityProvider";
 
 const EditModal = ({ isOpen, onClose, person }) => {
   const { sharesTotalQuantity} = useContext(SharesQuantityContext);
-  console.log(person);
+
+
+  console.log(isOpen);
   return (
     <Modal open={isOpen} onClose={onClose}>
       <ModalDialog sx={{ border: "solid #ED6930 2px", borderRadius: "10px" }}>
         <ModalClose />
-        <CreateOrEditShareholderForm sharesTotalQuantity = { sharesTotalQuantity } person = {person}/>
+        <CreateOrEditShareholderForm sharesTotalQuantity = { sharesTotalQuantity } person = {person} isPersonEditing = {isOpen}/>
       </ModalDialog>
     </Modal>
   );
